cc2026e3b7aff7d155de7a05677e62d068d011d3
Brijesh Rajbhar <brijeshrajbhar366@gmail.com>
2025-12-30 14:02:09 +0530

    Fix static files configuration

--- aac2025/settings.py
+++ aac2025/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v+5qaje@)c1j458bbzd7hark^kbdk787qph8zhvsp554t*&5*@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,8 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+import os
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# (optional but recommended)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
