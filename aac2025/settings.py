9d854b0047cf42e860b1452ca741c7f308412c2d
Brijesh Rajbhar <brijeshrajbhar366@gmail.com>
2025-12-30 14:02:48 +0530

    Fix static files configuration

--- aac2025/settings.py
+++ aac2025/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'v+5qaje@)c1j458bbzd7hark^kbdk787qph8zhvsp554t*&5*@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
